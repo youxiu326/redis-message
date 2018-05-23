2604c24e4416a6e69a90905ed2b693f6302c1841
lucongyue <jslcylcy@163.com>
2018-05-23 09:06:50 +0800

    优化代码

--- src/main/java/com/lcy/redismessage/conf/RedisConfig.java
+++ src/main/java/com/lcy/redismessage/conf/RedisConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.listener.PatternTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 
 /**
  * @author player six
--- src/main/java/com/lcy/redismessage/controller/TestController.java
+++ src/main/java/com/lcy/redismessage/controller/TestController.java
@@ -2,7 +2,6 @@ package com.lcy.redismessage.controller;
 
 import com.lcy.redismessage.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
